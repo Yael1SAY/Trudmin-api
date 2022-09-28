5486722ced7df784dbd5d6bd3a9ce9cf08ff13f8
Yael Salinas <yael.system10@gmail.com>
2022-09-28 13:57:31 -0500

    Se modifica modelo Servicio

--- src/main/java/com/trudmin/api/model/Servicio.java
+++ src/main/java/com/trudmin/api/model/Servicio.java
@@ -6,12 +6,12 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,8 +29,8 @@ import lombok.ToString;
 public class Servicio implements Serializable{
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_servicio")
+	@JsonIgnore
 	private long idServicio;
 	
 	//@Column(name = "empleado_id")
